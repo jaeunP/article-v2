57e85eb551fd5d577b9e2fd70546243cd8ab11b5
jaeunP <okn2432@gmail.com>
2023-10-16 18:45:44 +0900

    initial commit

---
+++ src/main/java/aticle/articlev2/ArticleV2Application.java
@@ -0,0 +1,13 @@
+package aticle.articlev2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArticleV2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ArticleV2Application.class, args);
+    }
+
+}
---
+++ src/test/java/aticle/articlev2/ArticleV2ApplicationTests.java
@@ -0,0 +1,13 @@
+package aticle.articlev2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArticleV2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
